d2457ad457e600de97ba38669ce51f98e3c6e53d
Blazkowicz <bonnet.nicolas66@gmail.com>
2024-10-15 17:19:16 +0200

    use .env var to database connection

--- src/modules/database.module.js
+++ src/modules/database.module.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const connection = new Sequelize('', '', '', {
-    host: '',
-    dialect: ''
+const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_ADDRESS,
+    dialect: 'mysql'
 });
 
 connection.authenticate().then(() => {
