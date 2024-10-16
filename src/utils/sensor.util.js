89f6b0315ff6e1e932365d3dcad9e82c61b114e4
Blazkowicz <bonnet.nicolas66@gmail.com>
2024-10-16 21:18:12 +0200

    Clean sensor util and add pingPort function

--- src/utils/sensor.util.js
+++ src/utils/sensor.util.js
@@ -1,39 +1,57 @@
 const { EVENTS } = require('../../constants')
 const io = require("socket.io-client");
+const net = require('net');
+const { resolve } = require('path');
+const { rejects } = require('assert');
 
 const SensorUtil = {}
 
+/**
+ * Get details from sensor
+ * @param {String} ip 
+ * @param {Integer} port 
+ * @param {String} key 
+ * @param {EVENTS} event 
+ * @returns 
+ */
 SensorUtil.getSystemDetail = (ip, port, key, event) => {
     return new Promise((resolve, reject) => {
         const socket = io(`http://${ip}:${port}`);
-        
-        // Émettre l'authentification
         socket.emit(EVENTS.AUTHENTICATE, key);
-
-        // Écouter la réponse d'authentification
         socket.on(EVENTS.AUTHENTICATE, (success) => {
             if (success) {
-                console.log("Connecté");
-                
-                // Demander des données après l'authentification
                 socket.emit(event);
-
-                // Écouter l'événement et retourner les données
                 socket.on(event, (data) => {
-                    console.log('Received data:', data);
-                    resolve(data);  // Retourne les données via la Promise
+                    resolve(data);
                 });
             } else {
-                console.log('Authentication failed');
                 reject('Authentication failed');
             }
         });
-
-        // Gérer les erreurs de connexion
         socket.on('connect_error', (err) => {
             reject(err);
         });
     });
 };
+
+/**
+ * Ping address:port to check if its available
+ * @param {String} ip 
+ * @param {Integer} port 
+ * @returns {Boolean}
+ */
+SensorUtil.pingPort = (ip, port) => {
+    return new Promise((resolve, rejects) => {
+        const socket = new net.Socket();
+        socket.setTimeout(1000);
+        socket.on('connect', function () {
+            resolve(true);
+        }).on('error', function (e) {
+            resolve(false);
+        }).on('timeout', function (e) {
+            resolve(false);
+        }).connect(port, ip)
+    });
+}
 
 module.exports = { SensorUtil }
